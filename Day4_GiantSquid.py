156f965d2876922cbfdb0187fa34243380ba8123
EngineerMinded <wamj283@gmail.com>
2021-12-21 13:38:52 -0500

    Start on Day 4

---
+++ Day4_GiantSquid.py
@@ -0,0 +1,14 @@
+'''
+Wayne Mack
+(c)2021 EngineerMinded
+
+
+Program: Depth Difference indicator
+Written in: Python
+------------------------------------------
+NOTE: This is an activity for:
+Advent of Code
+https://adventofcode.com/2021/day/4
+------------------------------------------
+
+'''
