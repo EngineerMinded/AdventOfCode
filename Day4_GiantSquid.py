31fa87cc28ed8d4525def1d02f6e4792b5f8d12e
EngineerMinded <wamj283@gmail.com>
2021-12-21 20:10:54 -0500

    Update Day4_GiantSquid.py

--- Day4_GiantSquid.py
+++ Day4_GiantSquid.py
@@ -12,15 +12,32 @@ https://adventofcode.com/2021/day/4
 ------------------------------------------
 
 '''
-cla
-class numberDrawing:
-
-    ''' This class will hold the numbers that are drawed '''
-
-    def populateNumbers (numberList):
-        for a in range(lens(numberList))
-
 class BingoCard:
-    ''' This class will hold the individual bingo cards '''
-    def _init_ (self, winningNumbers):
-        self.winningNumbers =
+    ''' These are individual bingo Card Objects '''
+    cardNumbers = []
+    winningSquares = []
+
+    def populateCards(dataInput):
+        cardNumbers = dataInput.split()
+        winningSquares = [0] * 25
+
+def fileRead(fileName):
+    file = open (fileName)
+    arrayToReturn = file.read().splitlines()
+    file.close()
+    return arrayToReturn
+
+def parseBingoData (inputData):
+    '''
+    The Bingo data consist of the called number as list level 0, use The
+    mathematic equation numberOfCards = (totalNumberOfItemsInList / 10) - 1
+    to make sense of parseing the data.
+
+    '''
+    selectedNumbers = inputdata[0].split(",")
+    numberOfCards = (len(inputData) / 10) - 1
+
+    bingocards = []
+    arrayaddressCounter = 1
+    for i in range(numberOfCards):
+        bingoCards[i] =
