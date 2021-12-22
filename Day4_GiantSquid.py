ec99a4818f3fa61afe9b94f81dfbf645eb08a93a
EngineerMinded <wamj283@gmail.com>
2021-12-21 21:31:27 -0500

    Update Day4_GiantSquid.py

--- Day4_GiantSquid.py
+++ Day4_GiantSquid.py
@@ -19,10 +19,15 @@ class BingoCard:
     gameWinningNumber = 0
 
     def __init__ (self, dataInput):
+        ''' Constructor '''
         cardNumbers = dataInput.split()
         winningSquares = [0] * 25
 
-    def addWinningNumber (winningNumber):
+    def addWinningNumberAndDetermineWinner (winningNumber):
+        '''
+        Add a number and determine if this card is the winner by returning
+        the next function
+        '''
         for x in range(len(cardNumbers)):
             if cardNumber[x] == winningNumber:
                 winningSquares[x] = 1
@@ -30,23 +35,36 @@ class BingoCard:
         return thereIsAWinner(winningNumber)
 
     def thereIsAWinner (winningNumber):
+        '''
+        Determine if any rows or columns have called Bingo
+        '''
         for u in range(5):
-            if winningSquares[u] == 1 and winningSquares [u + 5] = 1 and winningSquares [u + 10] = 1 and winningSquares [u + 15] = 1 and winningSquares [u + 20] = 1:
+            if winningSquares[u] == 1 and winningSquares [u + 5] == 1 and winningSquares [u + 10] == 1 and winningSquares [u + 15] == 1 and winningSquares [u + 20] == 1:
                 gameWinningNumber = winningNumber
                 return True
         for v in range (5):
-            if winningSquares[( u * 5) ] == 1 and winningSquares [( u * 5) + 1] = 1 and winningSquares [( u * 5) + 2] = 1 and winningSquares [( u * 5) + 3] = 1 and winningSquares [( u * 5) + 4] = 1:
+            if winningSquares[( u * 5) ] == 1 and winningSquares [( u * 5) + 1] == 1 and winningSquares [( u * 5) + 2] == 1 and winningSquares [( u * 5) + 3] == 1 and winningSquares [( u * 5) + 4] == 1:
                 gameWinningNumber = winningNumber
                 return True
+        # if the program made it here there was not row and now, it has been ruled as False
         return False
 
     def tallyWinningScore():
+        '''
+        If a winner is declared, tally up the score and
+        return the winning score. this is the objective
+        of the Program
+        '''
         returnAnswer = 0
         for i in range(25):
-            if winningSquare[i] = 0:
+            if winningSquare[i] == 0:
                 returnAnswer += winningSquare[i]
         return returnAnswer * gameWinningNumber
 def fileRead(fileName):
+    '''
+    read the file data and return it's contents
+    as an arrayToReturn
+    '''
     file = open (fileName)
     arrayToReturn = file.read().splitlines()
     file.close()
@@ -71,5 +89,12 @@ def parseBingoData (inputData):
         print (dataToBingoCard)
         bingoCards.append (BingoCard(dataToBingoCard))
         arrayAddressCounter += 5
+def bingoDrawingProcedure():
+    '''
+    This function will draw numbers from the array of winning
+    numbers until a winner in the BIngo cards are declared. it
+    will then return the winning tallyWinningScore
+    '''
+
 
 parseBingoData (fileRead ("Day4.PZL"))
