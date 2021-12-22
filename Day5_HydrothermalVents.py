cf41aeb175b1e357c828f25e51619e9c5000c4bd
EngineerMinded <wamj283@gmail.com>
2021-12-22 18:19:16 -0500

    Fix

--- Day5_HydrothermalVents.py
+++ Day5_HydrothermalVents.py
@@ -12,43 +12,10 @@ https://adventofcode.com/2021/day/5
 ------------------------------------------
 
 '''
-
-class Vector:
-    x1 = 0
-    x2 = 0
-    y1 = 0
-    y2 = 0
-    def __init__ (self, metaData):
-        self.metaData = metaData
-        parseData = metaData.split()
-        parseData[1] = ","
-        metaData = str(parseData[0]) + str(parseData[1]) + str(parseData[2])
-        parseData = metaData.split(",")
-        self.x1 = int(parseData[0])
-        self.y1 = int(parseData[1])
-        self.x2 = int(parseData[2])
-        self.y2 = int(parseData[3])
-    def addPoints (self,currentArray):
-        # iteration to disregard diagional lines
-        if (self.x1 != self.x2) and (self.y1 != self.y2):
-            return currentArray
-        if (self.x1 != self.x2):
-            if (self.x1 < self.x2):
-                for i in range(self.x1, self.x2 +1 ):
-                    currentArray.append(str(i) + "," + str(self.y1))
-            else:
-                for i in range(self.x2 +1 , self.x1):
-                    currentArray.append(str(i) + "," + str(self.y1))
-        elif(self.y1 != self.y2):
-            if (self.y1 < self.y2):
-                    for i in range(self.y1, self.y2 +1 ):
-                        currentArray.append(str(self.x1) + "," + str(i))
-            else:
-                for i in range(self.y2 +1 , self.y1):
-                    currentArray.append(str(self.x1) + "," + str(i))
-        else:
-            currentArray.append(str(self.x1) + "," + str(self.x2))
-        return currentArray
+vectorArray = []
+pointArray = []
+pointCounter = []
+Matrix = []
 
 def fileRead(fileName):
     '''
@@ -59,40 +26,50 @@ def fileRead(fileName):
     arrayToReturn = file.read().splitlines()
     file.close()
     return arrayToReturn
-vectorArray = []
-pointArray = []
-pointCounter = []
+
+def dechiperLineAndPlaceIntoMatrix(lineCommand):
+    global Matrix
+    intermediateArray = lineCommand.split()
+    firstVector = intermediateArray[0].split(",")
+    secondVector = intermediateArray[2].split(",")
+    x1 = int(firstVector[0])
+    y1 = int(firstVector[1])
+    x2 = int(secondVector[0])
+    y2 = int(secondVector[1])
+    #print (str(x1) + " " + str(y1) + " " + str(y1) + " " + str(y2))
+    if (x1 != x2) and (y1 != y2):
+        return
+    if (x1 != x2):
+        if (x1 < x2):
+            for i in range(x1, x2 + 1  ):
+                Matrix[i][y1]+= 1
+        else:
+            for i in range(x2 + 1  , x1):
+                Matrix[i][y1] += 1
+    elif(y1 != y2):
+        if (y1 < y2):
+                for i in range(y1, y2 + 1  ):
+                    Matrix[x1][i] += 1
+        else:
+            for i in range(y2 + 1  , y1):
+                Matrix[x1][1] += 1
+
+
 def PartOne():
-    global vectorArray
-    global pointArray
-    global pointCounter
-    rawData = fileRead("Day5.PZL")
+    # Generate Matrix at all zeroes
+    print ("Generating Matrix")
+    global Matrix
+    Matrix = [[0]* 10]*10
+    print (Matrix)
+    print ("Reading Data and placing into Matrix")
+    rawData = fileRead("Day5_TestCase.PZL")
     for i in range(len(rawData)):
-        vectorArray.append(Vector(rawData[i]))
-        pointArray = vectorArray[i].addPoints(pointArray)
-    # Now you have your point array and even duplicate. Let's sparce them to see which are duplicates
-    # start by adding the first One
-
-    def duplicatePointExists(pointToFind):
-        global pointCounter
-        for i in range(len(pointCounter)):
-            if pointCounter[i].pointExists(pointToFind):
-                return True
-        return False
-    #print (pointArray)
-    duplicateCount = 0
-    for i in range(len(pointArray)):
-        #print (str(i))
-        for j in range(len(pointArray)):
-            if (i != j and pointArray[i] == pointArray [j]):
-                alreadyExists = False
-                for k in range(len(pointCounter)):
-                    if pointCounter[k] == pointArray[i]:
-                        alreadyExists = True
-                if not alreadyExists:
-                    pointCounter.append(pointArray[i])
-                    duplicateCount += 1
-                    print (str(pointArray[i]) + " Found Duplicate : " + str(duplicateCount) + " of Total :" + str(i) + " / " + str(len(pointArray)))
-    print ("There are " + str(duplicateCount) + " duplicates")
-
+        dechiperLineAndPlaceIntoMatrix(rawData[i])
+    print (Matrix)
+    intersectCount = 0
+    for u in range(10):
+        for v in range(10):
+            if Matrix[u][v] > 1:
+                intersectCount+=1
+    print ("Number of intersecting points has been evaluated as : " + str(intersectCount))
 PartOne()
