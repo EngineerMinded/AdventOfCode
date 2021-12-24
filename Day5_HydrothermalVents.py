833bf1c7ad44f3f13339f4af9274178cd64d3115
EngineerMinded <wamj283@gmail.com>
2021-12-23 21:37:51 -0500

    Part 2

--- Day5_HydrothermalVents.py
+++ Day5_HydrothermalVents.py
@@ -11,6 +11,7 @@ Advent of Code
 https://adventofcode.com/2021/day/5
 ------------------------------------------
 
+ACHIEVMENT: OBTAINED FIRST GOLD STAR
 '''
 vectorArray = []
 pointArray = []
@@ -40,33 +41,54 @@ def dechiperLineAndPlaceIntoMatrix(lineCommand):
     y1 = int(firstVector[1])
     x2 = int(secondVector[0])
     y2 = int(secondVector[1])
+
     print (str(x1) + " " + str(y1) + " " + str(x2) + " " + str(y2))
     if (x1 != x2) and (y1 != y2):
-        print ("(Sequence not Used)")
+        # Diagonal Modification for Part 2
+        if abs(x2 - x1) == abs(y2 - y1):
+            countUpX = True
+            countUpY = True
+            if (x2 < x1):
+                countUpX = False
+            if (y2 < y1):
+                countUpY = False
+            for i in range(abs(x2 - x1) +1):
+                if countUpX:
+                    dx = x1 + i
+                else:
+                    dx = x2 - i
+                if countUpY:
+                    dy = y1 + i
+                else:
+                    dy = y2 - i
+                print str(dx) + " " + str(dy)
+                Matrix[dx][dy] +=1
+        else:
+            print ("(Sequence not Used)")
         return
     if (x1 != x2):
         if (x1 < x2):
             for i in range(x1, x2 +1, 1):
-                #print (str(i) + " " + str(y1))
+                print (str(i) + " " + str(y1))
                 Matrix[i][y1] +=1
                 numberOfAccesses +=1
         else:
-            for i in range(x1, x2 + 1, -1):
-                #print (str(i) + " " + str(y1))
+            for i in range(x1, x2 -1, -1):
+                print (str(i) + " " + str(y1))
                 Matrix[i][y1] +=1
                 numberOfAccesses +=1
     elif(y1 != y2):
         if (y1 < y2):
             for i in range(y1, y2 +1, 1  ):
-                #print (str(x1) + " " + str(i))
+                print (str(x1) + " " + str(i))
                 Matrix[x1][i] +=1
                 numberOfAccesses +=1
         else:
-            for i in range(y1, y2 + 1, -1):
-                #print (str(x1) + " " + str(i))
+            for i in range(y1, y2 - 1, -1):
+                print (str(x1) + " " + str(i))
                 Matrix[x1][i] +=1
                 numberOfAccesses +=1
-def PartOne():
+def Main():
     # Generate Matrix at all zeroes
     print ("Generating Matrix")
     global Matrix
@@ -85,4 +107,4 @@ def PartOne():
                 intersectCount+=1
     print ("Number of intersecting points has been evaluated as : " + str(intersectCount))
     print (numberOfAccesses)
-PartOne()
+Main()
