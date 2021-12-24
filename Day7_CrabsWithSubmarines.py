a9bcd8b94888f11743e384a57fee1c42c8deca66
EngineerMinded <wamj283@gmail.com>
2021-12-24 14:28:02 -0500

    Update Day7_CrabsWithSubmarines.py

--- Day7_CrabsWithSubmarines.py
+++ Day7_CrabsWithSubmarines.py
@@ -15,7 +15,6 @@ ACHIEVMENT: OBTAINED FIRST GOLD STAR
 '''
 fileName = ("Day7_TestCase.PZL")
 fileName = ("Day7.PZL")
-crabPositions = []
 def getFileInformation (fName):
     ''' Read Data from the file and, return as list to be evaluated '''
     File = open (fName)
@@ -23,7 +22,7 @@ def getFileInformation (fName):
     File.close()
     return returnThisList[0]
 
-def determineBestPosition(listFromSource):
+def determineBestPosition(listFromSource,trueForPart2):
     # get the minimum Number
     lowestNumber = 9999999999999
     highestNumber = -9999999999999
@@ -32,24 +31,36 @@ def determineBestPosition(listFromSource):
             lowestNumber = listFromSource[u]
         if listFromSource[u] > highestNumber:
             highestNumber = listFromSource[u]
-    def tallyTotalPoints (medianNumber, listFromSource):
+
+    def tallyTotalPoints (medianNumber, listFromSource, trueForPart2):
         totalPoints = 0
         for x in range(len(listFromSource)):
-            totalPoints += abs(listFromSource[x] - medianNumber)
+                # This loop determines fuel consumption for part one
+                totalPoints += abs(listFromSource[x] - medianNumber)
+                if trueForPart2:
+                    accumulator = 0
+                    for y in range(abs(listFromSource[x] - medianNumber) + 1):
+                        if y > 0:
+                            accumulator += y - 1
+                    totalPoints += accumulator
         return totalPoints
+
     bestNumber = 99999999999999
+    print (highestNumber - lowestNumber)
     for v in range(highestNumber - lowestNumber):
-        if tallyTotalPoints(v,listFromSource) < bestNumber:
-            bestNumber = tallyTotalPoints(v,listFromSource)
+        if tallyTotalPoints(v,listFromSource,trueForPart2) < bestNumber:
+            bestNumber = tallyTotalPoints(v,listFromSource,trueForPart2)
+            #print (str (v) + " " +str(bestNumber))
     return bestNumber
 
 
-def PartOne():
-    global crabPositions
+def Main(trueForPart2):
+    crabPositions = []
     getData = (getFileInformation(fileName).split(","))
     for n in range(len(getData)):
         crabPositions.append(int(getData[n]))
-    bestPoints = determineBestPosition(crabPositions)
+    bestPoints = determineBestPosition(crabPositions,trueForPart2)
     print ("The bestpoints points tallied are :" + str(bestPoints))
 
-PartOne()
+Main(False)
+Main(True)
