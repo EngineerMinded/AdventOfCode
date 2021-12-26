a9c75fea954b1d2176d8ea75f6b237f5b12bed2a
EngineerMinded <wamj283@gmail.com>
2021-12-26 17:27:02 -0500

    Update Day10_SyntaxScoring.py

--- Day10_SyntaxScoring.py
+++ Day10_SyntaxScoring.py
@@ -12,7 +12,6 @@ https://adventofcode.com/2021/day/10
 '''
 fileName = ("Day10.PZL")
 #fileName = ("Day10_TestCase.PZL")
-
 File = open(fileName)
 fileMetaData = File.read().splitlines()
 File.close()
@@ -20,40 +19,24 @@ totalSyntaxErrors = 0
 syntaxLines = []
 for a in range(len(fileMetaData)):
     syntaxLines.append(list(fileMetaData[a]))
-'''
- chunk Balance properties:
- 0 = "(" - 3 points
- 1 = "[" - 57 points
- 2 = "{" - 1197 points
- 3 = "<" - 25137 points
- '''
-
-
+    
 def syntaxCheck (lineNumber) :
-    #print ("Line Number" + str(lineNumber))
     chunkBalances = []
-    #print (chunkBalances)
     for a in range(len(syntaxLines[lineNumber])):
-        #print (str(chunkBalances) + " : " + syntaxLines[lineNumber][a])
         if syntaxLines[lineNumber][a] == "(" or syntaxLines[lineNumber][a] == "[" or syntaxLines[lineNumber][a] == "{" or syntaxLines[lineNumber][a] == "<":
             chunkBalances.append(syntaxLines[lineNumber][a])
         elif syntaxLines[lineNumber][a] == ")":
             if chunkBalances.pop() != "(":
-                #print ("Corrupted")
                 return 3
         elif syntaxLines[lineNumber][a] == "]":
             if chunkBalances.pop() != "[":
-                #print ("Corrupted")
                 return 57
         elif syntaxLines[lineNumber][a] == "}":
             if chunkBalances.pop() != "{":
-                #print ("Currupted")
                 return 1197
         elif syntaxLines[lineNumber][a] == ">":
             if chunkBalances.pop() != "<":
-                #print ("Corrupted")
                 return 25137
-    #print ("Not corrupted")
     syntaxLines[lineNumber] = chunkBalances
     return 0
 
@@ -63,12 +46,10 @@ for c in range(len(syntaxLines)):
         corruptedLines.append(c)
         totalSyntaxErrors += syntaxCheck(c)
 print("The syntax error total is :" + str(totalSyntaxErrors))
-#print("The corrupted Lines are: " + str(corruptedLines))
 previousNumber = 0
 for l in corruptedLines:
 
     syntaxLines[l] = []
-    #previousNumber = l
 ''' Begin procedures for Part 2 '''
 totalPoints = 0
 def remainingScoreDeterminationByLine (lineNumber):
