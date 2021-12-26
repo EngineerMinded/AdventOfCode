108fb121186fdf7a98a2e9f87c4077cdcbcfd27c
EngineerMinded <wamj283@gmail.com>
2021-12-26 16:41:24 -0500

    Update Day10_SyntaxScoring.py

--- Day10_SyntaxScoring.py
+++ Day10_SyntaxScoring.py
@@ -55,14 +55,64 @@ def syntaxCheck (lineNumber) :
                 print ("Corrupted")
                 return 25137
     print ("Not corrupted")
+    syntaxLines[lineNumber] = chunkBalances
     return 0
-
 
 corruptedLines = []
 for c in range(len(syntaxLines)):
     if syntaxCheck(c) != 0:
         corruptedLines.append(c)
         totalSyntaxErrors += syntaxCheck(c)
-
 print("The syntax error total is :" + str(totalSyntaxErrors))
 print("The corrupted Lines are: " + str(corruptedLines))
+previousNumber = 0
+for l in corruptedLines:
+
+    syntaxLines.pop(l - previousNumber)
+    previousNumber = l
+''' Begin procedures for Part 2 '''
+totalPoints = 0
+def remainingScoreDeterminationByLine (lineNumber):
+    totalbyLine = 0
+    charactertoFind = "X"
+    r = len(syntaxLines[lineNumber]) -1
+    while r >= 0:
+        if charactertoFind == "X":
+            totalbyLine *=5
+        g = syntaxLines[lineNumber][r]
+        if g == "(" :
+            if charactertoFind == "X":
+                totalbyLine += 1
+            elif charactertoFind =="(":
+                charactertoFind = "X"
+        elif g == "[":
+            if charactertoFind == "X":
+                totalbyLine += 2
+            elif charactertoFind =="[":
+                charactertoFind = "X"
+        elif g == "{":
+            if charactertoFind == "X":
+                totalbyLine += 3
+            elif charactertoFind =="{":
+                charactertoFind = "X"
+        elif g == "<":
+            if charactertoFind == "X":
+                totalbyLine += 4
+            elif charactertoFind =="<":
+                charactertoFind = "X"
+        elif g == ")":
+            charactertoFind = "("
+        elif g == "]":
+            charactertoFind = "["
+        elif g == "}":
+            charactertoFind = "{"
+        elif g == ">":
+            charactertoFind = "<"
+        print (totalbyLine)
+        r -= 1
+    print ("Final " + str(totalbyLine))
+    return totalbyLine
+
+part2Answer = 0
+for q in range(len(syntaxLines)):
+    part2Answer +=remainingScoreDeterminationByLine(q)
