1fa98c4036bec803580632515383e622f3da8d0a
EngineerMinded <wamj283@gmail.com>
2021-12-28 23:55:28 -0500

    Start of Day 12

---
+++ Day12_PassagePassing.py
@@ -0,0 +1,11 @@
+'''
+Wayne Mack
+(c)2021 EngineerMinded
+
+------------------------------------
+Day 11 : Passage Passing
+Written in: Python
+------------------------------------
+https://adventofcode.com/2021/day/12
+
+'''
