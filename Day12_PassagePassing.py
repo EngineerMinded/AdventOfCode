cc52e09b7cae80fdeca5ceb50ea5c8cba78b35c7
EngineerMinded <wamj283@gmail.com>
2021-12-29 22:03:56 -0500

    h

--- Day12_PassagePassing.py
+++ Day12_PassagePassing.py
@@ -14,22 +14,32 @@ https://adventofcode.com/2021/day/12
     of the projects '''
 class Cave :
 
-    def __init__ (self, iD) :
-        #initializing a new Cave
-        self.ID = iD
-        self.subCaves = []
-        numberOfEnds = 0
+    def __init__(self, *args):
+        self.firstSubID = "none"
+        if len(args) > 1:
+            self.ID = args[0]
+            self.firstSubID = [1]
+        else:
+            self.ID = args[0]
+
+
+        subCaves = []
+        if self.firstSubID != "none":
+            subCaves.append(Cave(self.firstSubID))
+        self.numberOfEnds = 0
+        print ("Cave :" + str(self.ID))
 
     def addCave(cave2BeAdded):
+        print ( iD + " :" + str(subcaves))
         # create a new cave. add it if it already does not exist
-        duplicateCave = False
         for u in subCaves:
+            foundExisting = False
             if u.ID == cave2BeAdded.ID:
                 for v in cave2BeAdded.subCaves:
                     u.addCave(v)
-                    duplicateCave = True
-            if not duplicateCave:
-                subCaves.append(cave2BeAdded)
+                    foundExisting = True
+        if not foundExisting:
+            subCaves.append(Cave(cave2BeAdded, "none"))
 
     def countEnds(endCount):
         # count caves that say End
@@ -39,5 +49,26 @@ class Cave :
             endCount = w.countEnds(endCount)
         return endCount
 
+
+fileName = ("Day12.Puzzle")
+fileName = ("Day12_TestCase1.Puzzle")
+fileName = ("Day12_TestCase2.Puzzle")
+fileName = ("Day12_TestCase3.Puzzle")
+
 commandList = []
 listOfCaves = []
+File = open(fileName)
+commandList = File.read().splitlines()
+File.close()
+for command in commandList:
+
+    commandData = command.split("-")
+    if (commandData[0] != "start"):
+        foundExisting = False
+        for w in listOfCaves:
+            if w.ID == commandData[0]:
+                command1 = commandData[1]
+                w.addCave(command1)
+                foundExisting = True
+        if not foundExisting:
+            listOfCaves.append(Cave(commandData[0],commandData[1]))
