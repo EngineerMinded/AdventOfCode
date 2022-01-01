55dbecc0762a303e8cf2b69c3a4b24c563c23304
Wayne's The Name <wamj283@gmail.com>
2022-01-01 12:42:59 -0500

    changfes

--- Day12_PassagePassing.py
+++ Day12_PassagePassing.py
@@ -12,38 +12,16 @@ https://adventofcode.com/2021/day/12
 ''' This is the first time we will be using a class
     in Python. This may start a trend for the remainder
     of the projects '''
-#!/usr/bin/python3
-import sys
-import itertools
-from collections import defaultdict, Counter, deque
 
-infile = sys.argv[1] if len(sys.argv)>1 else 'Day12.Puzzle'
-
-# adjacency list - for each vertex, what vertices does it have edges to?
-E = defaultdict(list)
-for line in open(infile):
-    a,b = line.strip().split('-')
-    E[a].append(b)
-    E[b].append(a)
-
-def solve(p1):
-    start = ('start', set(['start']), None)
-    ans = 0
-    Q = deque([start])
-    while Q:
-        # where we are, which small caves we've visited
-        pos, small, twice = Q.popleft()
-        if pos == 'end':
-            ans += 1
-            continue
-        for y in E[pos]:
-            if y not in small:
-                new_small = set(small)
-                if y.lower() == y:
-                    new_small.add(y)
-                Q.append((y, new_small, twice))
-            elif y in small and twice is None and y not in ['start', 'end'] and not p1:
-                Q.append((y, small, y))
-    return ans
-print(solve(p1=True))
-print(solve(p1=False))
+caveList = dict()
+fileName = ("Day12.Puzzle")
+fileName = ("Day12_TestCase.Puzzle")
+fileName = ("Day12_TestCase2.Puzzle")
+fileName = ("Day12_TestCase3.Puzzle")
+File = open(fileName)
+fileMetaData  = File.read().splitlines()
+print (fileMetaData)
+for a in fileMetaData:
+    data = (a.split("-"))
+    caveList.update({data[0] : data[1]})
+print (caveList)
