8812d944f191f8406a263e628e20ce49133d2d89
EngineerMinded <wamj283@gmail.com>
2022-12-02 17:43:12 -0500

    Day 2 Completeion

---
+++ 2022/Day 2/Program.cs
@@ -0,0 +1,49 @@
+﻿/*******************************************
+ * Wayne Mack                              *
+ * Advent of Code 2022 - Day 2             *
+ * Written in: C#                          *
+ * Main Program                            *
+ *******************************************/
+
+using System;
+namespace Day2_AOC_2022
+{
+    class Program
+    {
+        static RockPaperScissors rockPaperScissors;
+        static void Main(string[] args)
+        {
+            rockPaperScissors = new RockPaperScissors();
+
+            foreach (string line in System.IO.File.ReadAllLines("/Users/waynemack/Projects/Day2-AOC_2022/Day2-AOC_2022/Day2Data.txt"))
+            {
+                SELECTION getSelection(string item)
+                {
+                    switch (item)
+                    {
+                        case "A":
+                            return SELECTION.A;
+                        case "B":
+                            return SELECTION.B;
+                        case "C":
+                            return SELECTION.C;
+                        case "X":
+                            return SELECTION.X;
+                        case "Y":
+                            return SELECTION.Y;
+                        default:
+                            return SELECTION.Z;
+                    }
+                }
+                String [] readLine  = line.Split(" ");
+                rockPaperScissors.add(getSelection(readLine[1]), getSelection(readLine[0]));
+                
+            }
+            rockPaperScissors.printData();
+            // Part 1 Answer:
+            Console.WriteLine("The total of all games is :" + rockPaperScissors.getTotal());
+            // Part 2 Answer;
+            Console.WriteLine("The total number by the second set of rules is : " + rockPaperScissors.root.secondPredeterminiteValue());
+        }
+    }
+}
---
+++ 2022/Day 2/RockPaperScissors.cs
@@ -0,0 +1,225 @@
+﻿/*******************************************
+ * Wayne Mack                              *
+ * Advent of Code 2022 - Day 2             *
+ * Written in: C#                          *
+ * Rock Paper Scissors object class        *
+ *******************************************/
+
+using System;
+namespace Day2_AOC_2022
+{
+    public enum SELECTION { A, B, C, X, Y, Z };
+    public class RockPaperScissors
+    {
+        public Node root;
+        public void add(SELECTION t, SELECTION u)
+        {
+            if (root == null)
+            {
+                root = new Node(u, t);
+            }
+            else
+            {
+                root.add(u, t);
+            }
+        }
+        public void printData()
+        {
+            root.printData();
+        }
+        public int getTotal()
+        {
+            return root.getTotal();
+        }
+    }
+    public class Node
+    {
+        SELECTION you;
+        SELECTION them;
+        int points;
+        Node next;
+
+        public Node(SELECTION t, SELECTION u) 
+        {
+            this.you = u;
+            this.them = t;
+            next = null;
+            points = assignPoints();
+        }
+        public void add(SELECTION t, SELECTION u)
+        {
+            if (next != null)
+            {
+                next.add(t, u);
+            }
+            else
+            {
+                next = new Node(t,u);
+            }
+        }
+        private int assignPoints()
+        {
+            /* Scoring Criteria:
+            * 1 - Rock
+            * 2 - Paper
+            * 3 - Scissors
+            * PLUS:
+            * 0 - Lost
+            * 3 - Draw
+            * 6 - Won
+            */
+            int score = 0;
+            switch (you)
+            {
+                case SELECTION.X:
+                    {
+                        score = score + 1;
+                        if (them == SELECTION.A)
+                        {
+                            score = score + 3;
+                        }
+                        else if (them == SELECTION.C)
+                        {
+                            score = score + 6;
+                        }
+                        break;
+                    }
+                    
+                case SELECTION.Y:
+                    {
+                        score = score + 2;
+                        if (them == SELECTION.B)
+                        {
+                            score = score + 3;
+                        }
+                        else if (them == SELECTION.A)
+                        {
+                            score = score + 6;
+                        }
+                        break;
+                    }      
+                case SELECTION.Z:
+                    {
+                        score = score + 3;
+                        if (them == SELECTION.C)
+                        {
+                            score = score + 3;
+                        }
+                        else if (them == SELECTION.B)
+                        {
+                            score = score + 6;
+                        }
+                        break;
+                    }       
+            }
+            return score;
+        }
+        public int getPoints()
+        {
+            return points;
+        }
+        public void printData()
+        {
+            Console.WriteLine(you + " " + them + " " + points);
+            if (!(next == null)) {
+                next.printData();
+            }
+        }
+        public int getTotal ()
+        {
+            if (next != null)
+            {
+                return points + next.getTotal();
+            }
+            else
+            {
+                return points;
+            }
+        }
+        public int secondPredeterminiteValue ()
+        {
+            int score = 0;
+            /* 'Them' value will be substituted as winning determinite
+             *
+             * X - You must lose at losing value
+             * 
+             * Y - You must tie at same value
+             * 
+             * Z - You must win at winning value */
+
+            if (you == SELECTION.X)
+            {
+                switch (them)
+                {
+                    case SELECTION.A:
+                        {
+                            score = score + 3;
+                            break;
+                        }
+                    case SELECTION.B:
+                        {
+                            score = score + 1;
+                            break;
+                        }
+                    default:
+                        {
+                            score = score + 2;
+                            break;
+                        }
+                }
+            }
+            else if (you == SELECTION.Y)
+            {
+                score = score + 3;
+                switch (them)
+                {
+                    case SELECTION.A:
+                        {
+                            score = score + 1;
+                            break;
+                        }
+                    case SELECTION.B:
+                        {
+                            score = score + 2;
+                            break;
+                        }
+                    default:
+                        {
+                            score = score + 3;
+                            break;
+                        }
+                }
+            }
+            else if (you == SELECTION.Z)
+            {
+                score = score + 6;
+                switch (them)
+                {
+                    case SELECTION.A:
+                        {
+                            score = score + 2;
+                            break;
+                        }
+                    case SELECTION.B:
+                        {
+                            score = score + 3;
+                            break;
+                        }
+                    default:
+                        {
+                            score = score + 1;
+                            break;
+                        }
+                }
+            }
+            if (next == null)
+            {
+                return score;
+            }
+            else
+            {
+                return score + next.secondPredeterminiteValue();
+            } 
+        }
+    }
+}
