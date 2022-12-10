51de08f6248936505f7c2f17d22e5eeec1891dc1
EngineerMinded <wamj283@gmail.com>
2022-12-10 09:33:48 -0500

    Started Day 7

---
+++ 2022/Day 7 - No Space Left On Drive/DirectoryList.cs
@@ -0,0 +1,117 @@
+﻿class DirectoryList
+{
+    string dir;
+    int value;
+    DirectoryList next;
+
+    public  DirectoryList() 
+    {
+        dir = "/";
+        next = null;
+    }
+
+    public DirectoryList (string dir, int value)
+    {
+        this.dir = dir;
+        this.value = value;
+        next = null; 
+    }
+
+    public void cd(string variable)
+    {
+        if (variable == "/")
+        {
+            next = null;
+        }
+        if (variable == "..")
+        {
+            if (next != null )
+            {
+                if (next.next == null)
+                {
+                    next = null;
+                }
+                else
+                {
+                    next.cd(variable);
+                }
+            }
+        }
+        else if (next != null)
+        {
+            next.cd(variable);
+        }
+        else
+        {
+            dir = variable;
+        }
+    }
+    public string getWorkingDirectory()
+    {
+        if (next != null)
+        {
+            return next.getWorkingDirectory();
+        }
+        else
+        {
+            return dir;
+        }
+    }
+
+   
+    public void addValue (string directory,int Value )
+    {
+        if ( dir == directory)
+        {
+            this.value = this.value + Value;
+        }
+        else
+        {
+            if (next != null)
+            {
+                next.addValue(directory,Value);
+            }
+            else
+            {
+                next = new DirectoryList(dir,value);
+            }
+        }
+    }
+    public void addValue (string directory, String fromDirectory ,DirectoryList getInfo)
+    {
+        if (dir == directory )
+        {
+            if (getInfo.dir == fromDirectory)
+            {
+                this.value = this.value + getInfo.value;
+            }
+            else
+            {
+                if (getInfo.next != null)
+                {
+                    addValue(directory,fromDirectory,getInfo.next);
+                }
+            }
+        }
+        else
+        {
+            if (next != null)
+            {
+                next.addValue (directory,fromDirectory,getInfo);
+            }
+            else
+            {
+                next = new DirectoryList(directory, value);
+            }
+        }
+    }
+    void printAll()
+    {
+        Console.WriteLine(dir + " " + value);
+        if (next != null)
+        {
+            next.printAll();
+        }
+    }
+}
+
---
+++ 2022/Day 7 - No Space Left On Drive/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+enum Mode { INSERTION_MODE, NONE}
+class Program
+{
+    static DirectoryList workingDirectory;
+    static FileObject objectList;
+
+    static void Main(string[] args)
+    {
+
+        string path = "C:\\Users\\wamj2\\OneDrive\\Desktop\\Advent Of Code 2022\\Day 7 - No Space Left On Device\\Day 7 - No Space Left On Device\\Day7Sample.txt";
+        objectList = new FileObject();
+        workingDirectory = new DirectoryList();
+
+        foreach (string line in File.ReadLines(path))
+        {
+            if (line.Contains("$"))
+            {
+                if (line.Contains ("cd"))
+                {
+                    workingDirectory.cd(line.Split(" ")[2]);
+                }
+                else if (line.Contains("ls"))
+                {
+
+                }
+            }
+            else
+            {
+                objectList.append(workingDirectory.getWorkingDirectory(), line);
+            }
+        }
+        objectList.display();
+
+        DirectoryList summaryDirectoryList = new DirectoryList();
+        summaryDirectoryList.append()
+
+    }
+}
