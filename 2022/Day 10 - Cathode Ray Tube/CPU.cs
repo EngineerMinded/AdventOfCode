e02228730851ff9f832a6dd53a81e5a2203a094c
EngineerMinded <wamj283@gmail.com>
2022-12-24 23:37:02 -0500

    Completed Day 10

---
+++ 2022/Day 10 - Cathode Ray Tube/CPU.cs
@@ -0,0 +1,89 @@
+﻿/*************************************************************************
+ * Wayne Mack                                                            *
+ * Advent of Code Day 10 - Cathode Ray Tube                              *
+ * Written in: C#                                                        *
+ * --------------------------------------------------------------------- *
+ * CPU.cs                                                                *
+ *************************************************************************/
+
+using System;
+class CPU
+{
+    char currentCharacter;
+    int arrayCounter, arrayLength;
+    bool drawNextOne, changeValue;
+    char[] data;
+    public int getCountAt(int getCountHere, string [] commandList, int arrayLength, bool partOne)
+    {
+
+        int currentValue = 1;
+        arrayCounter = 0;
+        int nextAccumulator =0;
+        bool halfPhase = false;
+        changeValue = true;
+        data = new char[240];
+        for (int o = 0; o < 240; o++ ){
+            data[o] = '.';
+        }
+        
+
+        for (int i = 0; i < getCountHere - 1; i++)
+        {
+            
+            // PIXEL DETERMINATION GOES HERE///
+            int j = i;
+            while (j > 40)
+            {
+                j = j - 40; 
+            }
+            j++;
+            if ((currentValue == j) || (currentValue + 1  == j) || (currentValue + 2 == j ))
+            {
+                data[i] = '#';
+            }
+            //Console.WriteLine(currentValue + " " + j + " " + data[i]);
+            if (halfPhase)
+            {
+                currentValue = currentValue + nextAccumulator;
+                halfPhase = false;
+            }
+            else
+            {
+                if (!commandList[arrayCounter].Contains("noop"))
+                {
+                    string currentCharacter = commandList[arrayCounter].Split(" ")[1];
+                    nextAccumulator = int.Parse(currentCharacter);
+                    halfPhase = true;
+                    
+                }
+                else
+                {
+                    halfPhase = false;
+                }
+                arrayCounter++;
+                if (arrayCounter >= arrayLength)
+                {
+                    arrayCounter = 0;
+                }
+            }
+
+
+        }
+        if (partOne)
+        {
+            Console.WriteLine(getCountHere + " x " + currentValue + " = " + getCountHere * currentValue);
+            return getCountHere * currentValue;
+        }
+        else
+        {
+            return currentValue;
+        }
+    }
+    public char[] getData(int getCountHere, string[] commandList, int arrayLength, bool partOne)
+    {
+        int m = getCountAt(getCountHere, commandList, arrayLength, partOne);
+        return data;
+    }
+
+};
+
---
+++ 2022/Day 10 - Cathode Ray Tube/Program.cs
@@ -0,0 +1,71 @@
+﻿/*************************************************************************
+ * Wayne Mack                                                            *
+ * Advent of Code Day 10 - Cathode Ray Tube                              *
+ * Written in: C#                                                        *
+ * --------------------------------------------------------------------- *
+ * Program.cs                                                            *
+ *************************************************************************/
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        CPU cpu = new CPU();
+        string command;
+        int countForArray = 0;
+        string fileName = "C:\\Users\\wamj2\\OneDrive\\Desktop\\Advent Of Code 2022\\Day 10 - Cathode Ray Tube\\Day 10 - Cathode Ray Tube\\Day10Data.txt";
+
+        string [] commandList = new string[150];
+        // Read from the text file
+        foreach (string line in File.ReadLines(fileName))
+        {
+            commandList[countForArray] = line;
+            countForArray++;
+        }
+        ///////////////////////////////////////////////////PART ONE //////////////////////////////////////////////////////////////////
+        int total = 0; 
+       
+        total = total + (cpu.getCountAt(20, commandList, countForArray, true));
+        total = total + (cpu.getCountAt(60, commandList, countForArray, true));
+        total = total + (cpu.getCountAt(100, commandList, countForArray, true));
+        total = total + (cpu.getCountAt(140, commandList, countForArray, true));
+        total = total + (cpu.getCountAt(180, commandList, countForArray, true));
+        total = total + (cpu.getCountAt(220, commandList, countForArray, true));
+
+        Console.WriteLine("The sum of Part One is : " + total);
+        ///////////////////////////////////////////////////PART TWO///////////////////////////////////////////////////////////////////
+        char[] image = cpu.getData(240, commandList, countForArray, false);
+        Console.WriteLine();
+  
+        for (int x = 0; x < 40; x++) 
+        {
+            Console.Write(image[x]);
+        }
+        Console.WriteLine();
+        for (int x = 40; x < 80; x++)
+        {
+            Console.Write(image[x]);
+        }
+        Console.WriteLine(); 
+        for (int x = 80; x < 120; x++)
+        {
+            Console.Write(image[x]);
+        }
+        Console.WriteLine(); 
+        for (int x = 120; x < 160; x++)
+        {
+            Console.Write(image[x]);
+        }
+        Console.WriteLine(); 
+        for (int x = 160; x < 200; x++)
+        {
+            Console.Write(image[x]);
+        }
+        Console.WriteLine(); 
+        for (int x = 200; x < 240; x++)
+        {
+            Console.Write(image[x]);
+        }
+        Console.WriteLine();
+    }
+}
