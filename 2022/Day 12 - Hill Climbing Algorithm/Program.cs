88e3016ace2a05ece8be27c41e3d7029a0034918
EngineerMinded <wamj283@gmail.com>
2023-01-05 23:55:08 -0500

    Closer to an answer for day 12

--- 2022/Day 12 - Hill Climbing Algorithm/Program.cs
+++ 2022/Day 12 - Hill Climbing Algorithm/Program.cs
@@ -1,16 +1,17 @@
-﻿/**************************************************
+/**************************************************
  * Wayne Mack                                     *
  * Day 12 - Hill Climbing Algorithm               *
  * -----------------------------------------------*
  * Written in C#                                  *
- * (((( UNFINISHED )))))))                        *
- **************************************************/ 
+ * Gets the right answer for sample but goes      *
+ * Forever for practical                          *
+ **************************************************/
 class Program
 {
     static bool example = true;
-    static int[,]? topography;
+    static int[,]? Map, Trail;
     static string? path;
-    static List<string> stepList;
+
 
     static void Main(string[] args)
     {
@@ -20,8 +21,7 @@ class Program
         {
             path = "Day12Example.txt";
         }
-        topography = new int[File.ReadAllLines(path).Length, File.ReadAllLines(path)[0].ToCharArray().Length];
-        stepList = new List<string>();
+        Map = new int[File.ReadAllLines(path).Length, File.ReadAllLines(path)[0].ToCharArray().Length];
 
         // Populate topography with characters
 
@@ -32,16 +32,16 @@ class Program
             y = 0;
             foreach (char c in line.ToCharArray())
             {
-                topography[x, y] = (int) c - ( 96);
+                Map[x, y] = (int) c - ( 96);
                 if (c == 'S')
                 {
                     startingPoint = (x, y);
-                    topography[x, y] = 0; 
+                    Map[x, y] = 0;
                 }
                 if (c == 'E')
                 {
                     endingPoint = (x, y);
-                    topography[x, y] = 27;
+                    Map[x, y] = 27;
                 }
 
                 y++;
@@ -49,143 +49,81 @@ class Program
             x++;
         }
 
-        
-
         for (int i = 0; i < x; i++)
         {
             for (int j = 0; j < y; j++)
             {
-                if (topography[i, j] < 10) { Console.Write('0'); }
-                Console.Write(topography[i, j] + " ");
+                if (Map[i, j] < 10) { Console.Write('0'); }
+                Console.Write(Map[i, j] + " ");
             }
             Console.WriteLine("\n");
         }
 
-        Console.WriteLine("The Answer to Part One is: " + getPathOfLeastSteps(startingPoint.x, startingPoint.y, new List<(int, int)>() ));
-        stepList.Reverse();
-        foreach (String line in stepList)
+
+        void displayTrail()
         {
-            Console.WriteLine(line);
+            for (int i = 0; i < Trail.GetLength(0); i++)
+            {
+                for (int j = 0; j < Trail.GetLength(1); j++)
+                {
+                    Console.Write(Trail[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
 
-        int getPathOfLeastSteps (int xStep, int yStep, List <(int, int)> stepsAlreadyTaken)
+        Trail = new int[Map.GetLength(0), Map.GetLength(1)];
+        for (int i = 0; i < Trail.GetLength(0); i++)
         {
-            if (topography[xStep,yStep] == 27)
+            for (int j =0; j < Trail.GetLength(1); j++)
             {
-                return 1;
-            }
-            
-            if (deadEnd(xStep, yStep))
-            {
-                Console.WriteLine("Found Dead End!");
-                return 99999999;
-            }
-            
-            List<(int, int)> currentList = stepsAlreadyTaken;
-            currentList.Add((xStep, yStep));
-
-            int North, South, East ,West; 
-            North = South = East = West = 999999999;
-
-            if (xStep > 0)
-            {
-                if (!hasBeenFound (xStep - 1, yStep))
-                {
-                    if (isAcessible(xStep - 1, yStep))
-                    {
-                        North =  getPathOfLeastSteps(xStep - 1, yStep, currentList);
-                    }
-                }
-            }
-            if (xStep < topography.GetLength(0) - 1)
-            {
-                if (!hasBeenFound(xStep + 1, yStep))
-                {
-                    if (isAcessible(xStep + 1, yStep))
-                    {
-                        South = 1 + getPathOfLeastSteps(xStep + 1, yStep, currentList);
-                    }
-                }
-            }
-            if (yStep > 0)
-            {
-                if (!hasBeenFound(xStep , yStep - 1))
-                {
-                    if (isAcessible(xStep , yStep - 1))
-                    {
-                        East =  1 + getPathOfLeastSteps(xStep , yStep - 1, currentList);
-                    }
-                }
-            }
-            if (yStep < topography.GetLength(1) - 1)
-            {
-                if (!hasBeenFound(xStep , yStep + 1))
-                {
-                    if (isAcessible(xStep , yStep + 1))
-                    {
-                        West =  1 + getPathOfLeastSteps(xStep, yStep + 1, currentList);
-                    }
-                }
-            }
-            String gotoHere = "";
-            int Answer = North; gotoHere = "North";
-            if (South < Answer) { Answer = South;  gotoHere = "South"; }
-            if (East < Answer) { Answer = East;  gotoHere = "East"; }
-            if (West < Answer) { Answer = West; gotoHere = "West"; }
-            if ((xStep, yStep) == endingPoint) { gotoHere = "End"; }
-            stepList.Add(xStep + " , " + yStep + " " + gotoHere + " " + stepCount++);
-            return  1 + Answer;
-
-            bool hasBeenFound (int xTest, int yTest)
-            {
-                foreach ((int,int) fn in stepsAlreadyTaken)
-                {
-                    if (fn == (xTest,yTest))
-                    {
-                        return true;
-                    }
-                    
-                }
-                return false;
-            }
-
-            bool isAcessible (int xTest, int yTest)
-            {
-                return ((topography[xTest,yTest] <= topography[xStep,yStep] + 1) 
-                    && ( !hasBeenFound(xTest,yTest))) ;
-            }
-            bool deadEnd (int xTest, int yTest)
-            {
-               if (xTest > 0)
-                {
-                    if (isAcessible (xTest  - 1, yTest))
-                    {
-                        return false;
-                    }
-                }
-               if (xTest < topography.GetLength(0) - 1)
-                {
-                    if (isAcessible(xTest + 1, yTest))
-                    {
-                        return false;
-                    }
-                }
-                if (yTest > 0)
-                {
-                    if (isAcessible(xTest, yTest - 1))
-                    {
-                        return false;
-                    }
-                }
-                if (yTest < topography.GetLength(1) - 1)
-                {
-                    if (isAcessible(xTest, yTest + 1))
-                    {
-                        return false;
-                    }
-                }
-                return true;
+                Trail[i, j] = Trail.GetLength(0) * Trail.GetLength (1);
             }
         }
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        void generateSteps(int xHere, int yHere, int Step)
+        {
+            //Console.Clear();
+            //displayTrail();
+            Console.WriteLine("Generating " + xHere + " , " + yHere + " of Area" + Map.GetLength(0) +" , " + Map.GetLength(1) +" as " + Step );
+            bool canAccess(int xAccess, int yAccess)
+            {
+                return ((Map[xAccess, yAccess] - Map[xHere, yHere] <= 1) && (Trail[xAccess,yAccess] >= Trail[xHere, yHere]));
+            }
+            Trail[xHere, yHere] = Step;
+            if (xHere > 0)
+            {
+                if (canAccess (xHere - 1, yHere)) {
+                    generateSteps(xHere - 1, yHere, Step + 1) ;
+                }
+            }
+            if (xHere < Map.GetLength(0) - 1)
+            {
+                if (canAccess(xHere + 1, yHere))
+                {
+                    generateSteps(xHere + 1, yHere, Step + 1);
+                }
+            }
+            if (yHere > 0)
+            {
+                if (canAccess(xHere, yHere - 1))
+                {
+                    generateSteps(xHere, yHere - 1, Step + 1);
+                }
+            }
+            if (yHere < Map.GetLength(1) - 1)
+            {
+                if (canAccess(xHere, yHere + 1))
+                {
+                    generateSteps(xHere, yHere + 1, Step + 1);
+                }
+            }
+        }
+        generateSteps(startingPoint.x, startingPoint.y, 0);
+
+        displayTrail();
+
+        Console.WriteLine ("The miminum steps on this trail are :" + Trail[endingPoint.x, endingPoint.y]);
     }
 }
