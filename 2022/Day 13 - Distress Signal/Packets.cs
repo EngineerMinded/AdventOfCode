f2623238c7af719039290cf712d696982e9515bb
EngineerMinded <wamj283@gmail.com>
2023-01-20 23:03:07 -0500

    Day 13 - First draft

---
+++ 2022/Day 13 - Distress Signal/Packets.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13___Distress_Signal
+{
+    internal class Packet
+    {
+        int leftNumbericValue, rightNumbericValue;
+        public List <string> left, right;
+        int leftValue, rightValue;
+        bool lastWasBracket;
+      
+
+        // subPacket Constructor
+        public Packet(string metadata)
+        {
+            left = new List <string> ();
+            right = new List <string> ();
+            lastWasBracket = false;
+            foreach (char leftChar in metadata.Split("\n")[0]) {
+                fillInformation(true, leftChar);   
+            }
+            foreach (char rightChar in metadata.Split("\n")[1])
+            {
+                fillInformation(false, rightChar);
+            }
+        }
+
+        private void fillInformation (bool LeftTrueRightFalse, char item)
+        {
+            void addData(bool isLeft, String data)
+            {
+                if (isLeft)
+                {
+                    if (data == null) { data = leftValue.ToString(); leftValue = 0; }
+                    left.Add(data);
+                }
+                else
+                {
+                    if (data == null) { data = rightValue.ToString(); rightValue = 0; }
+                    right.Add(data);
+                }
+            }
+            string addThis;
+            switch (item)
+            {
+                case '[':
+                    {
+                        addData(LeftTrueRightFalse, "[");
+                        break;
+                    }
+                case ']':
+                    {
+                        if (!lastWasBracket)
+                        {
+                            addData(LeftTrueRightFalse, null);
+                        }
+                        
+                        addData(LeftTrueRightFalse, "]");
+                        break;
+                    }
+                case ',':
+                    {
+                        if (lastWasBracket)
+                        {
+                            lastWasBracket = true;
+                        }
+                        else
+                        {
+                            addData(LeftTrueRightFalse, null);
+                        }
+                        break;
+                    }
+
+                default :
+                    { 
+                        if (LeftTrueRightFalse)
+                        {
+                            leftValue = leftValue * 10;
+                            leftValue = leftValue + (int)item - 48;
+                        }
+                        else
+                        {
+                            rightValue = rightValue * 10;
+                            rightValue = rightValue + (int)item - 48;
+                        }
+                        break;
+                    }
+            }
+            lastWasBracket = (item == ']');
+           
+
+        }
+        
+        public bool inRightOrder ()
+        {
+            leftNumbericValue = rightNumbericValue = 0;
+
+            void getNextValueForBoth()
+            {
+
+            }
+
+            return true;
+        }
+
+       
+        
+
+    }
+}
---
+++ 2022/Day 13 - Distress Signal/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13___Distress_Signal
+{
+    internal class Program
+    {
+        static List<Packet> packets;
+        static string fileName = "Day13Data.txt";
+        static bool test = true;
+
+        static void Main (string[] args)
+        {
+            packets = new List<Packet> ();
+            if (test)
+            {
+                fileName = "Day13Example.txt";
+            }
+            // GET INFORMATION FROM FILE AND PUT IN PACKET LIST
+            foreach (string data in File.ReadAllText(fileName).Split("\r\n\r\n"))
+            {
+                Console.WriteLine (data);
+                packets.Add(new Packet(data));
+            }
+
+            foreach (Packet packet in packets)
+            {
+                foreach (string w in packet.left)
+                {
+                    Console.Write(" " + w);
+                }
+                Console.WriteLine();
+                foreach (string w in packet.right)
+                {
+                    Console.Write(" " + w);
+                }
+                Console.WriteLine();
+            }      
+        }
+    }
+}
