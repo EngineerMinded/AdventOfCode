ade70ed5b842c620babeb153c881f23cd2eb9a7d
Wayne Mack <wamj283@gmail.com>
2025-12-07 13:01:28 -0500

    Remove inefficient part2 function
    Removed inefficient part2 function for beam path calculation and added a note for a revised approach.

--- 2025/day7.js
+++ 2025/day7.js
@@ -45,33 +45,6 @@ function part1(data) {
     //data.forEach((line) => {console.log(line);})
     return returnThis
 }
-/********************************************************
- * THIS IS NOT EFFICENT FOR BIG PUZZLES                 *
- * IT WILL TAKE FOREVER. GOTTA FIND ANOTHER WAY!        *
- ********************************************************/
-function part2(data) {
-    const ROWS = data[0].length;   // number of rows in each column
-    const COLUMNS = data.length;   // number of columns
-    let totalPaths = 0;
-
-    function beamSegment(column, row) {
-        // Base case: beam has reached beyond the last column
-        if (column >= COLUMNS) return 1;
-        let paths = 0;
-        const cell = data[column][row];
-        if (cell === '^') {
-            // Move diagonally up-right
-            if (row > 0) paths += beamSegment(column + 1, row - 1);
-            // Move diagonally down-right
-            if (row < ROWS - 1)  paths += beamSegment(column + 1, row + 1);
-            else  paths += beamSegment(column + 1, row);
-        return paths;
-    }
-
-    // Start beams wherever there's an 'S' in the first column
-    for (let row = 0; row < ROWS; row++) if (data[0][row] === 'S') totalPaths += beamSegment(0, row);
-    return totalPaths;
-}
 
 function part2Revised (data){
     /******************************************
